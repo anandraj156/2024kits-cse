7f6c50e39d474ab7876a7eb1b12e620e72c6e55c
anandraj156 <manchalaanandraju@gmail.com>
2024-06-20 16:18:28 +0530

    takes numeric input from the user, stores these numbers in an array, and dynamically displays them as a list on the web page

---
+++ script-numbers.js
@@ -0,0 +1,12 @@
+var number_list = [];
+function doAdd(){
+    let number = parseInt(document.getElementById('Number').value);
+    number_list.push(number);
+    let items = '';
+    for(let n of number_list) {
+        items=items+`<LI class="p-2 btn btn-primary m-2" 
+        style="list-style-type: none;">${n}</LI>`
+    }
+    let number_html = `<UL class="d-flex flex-wrap">${items}</UL>`
+    document.getElementById("numbers-list").innerHTML = number_html;
+}
